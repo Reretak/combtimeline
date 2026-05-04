db815f095fa3b8910bc0396dcc926b9218e31202
Reretak <rere-botak@proton.me>
2026-05-04 11:01:20 +0700

    furstrating:

--- index.js
+++ index.js
@@ -345,7 +345,7 @@ async function checkPw(pw, hashed){
     return false
   }
 }
-app.use(express.static(path.join(__dirname, 'client', 'dist')));
+app.use(express.static(path.join(__dirname, 'app', 'client', 'dist')));
 app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, 'app', 'client', 'dist', 'index.html'));
 });
